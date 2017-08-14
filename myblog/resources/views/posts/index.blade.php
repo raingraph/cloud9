c19ddc0c966ccf923dfd5993f1731e8967c23513
Yukii <cloudygraph@yahoo.co.jp>
2017-08-14 06:30:21 +0000

    first commit

---
+++ myblog/resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <!--<meta http-equiv="X-UA-Compatible" content="ie=edge">-->
+    <title>Blog posts</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Posts</h1>
+        <ul>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+        </ul>
+    </div>
+</body>
+</html>
